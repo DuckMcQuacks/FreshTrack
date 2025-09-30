1174f8e3897c7c9c81e15bc929e4bc39d0646d18
DuckMcQuacks <118669854+DuckMcQuacks@users.noreply.github.com>
2025-09-30 22:26:25 +0200

    Updated the landing page

--- FreshTrack_App/app/index.jsx
+++ FreshTrack_App/app/index.jsx
@@ -3,12 +3,18 @@ import React from 'react'
 
 const Home = () => {
   return (
-    <View>
-      <Text>Home</Text>
+    <View style={styles.container}>
+      <Text>Landing Page</Text>
     </View>
   )
 }
 
 export default Home
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  container : {
+    flex: 1,
+    alignItems: `center`,
+    justifyContent: `center`
+  }
+})
