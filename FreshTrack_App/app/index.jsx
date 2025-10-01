97299a14d211837d9cc121297932fe6012e757e3
DuckMcQuacks <118669854+DuckMcQuacks@users.noreply.github.com>
2025-10-01 21:40:14 +0200

    Added a new page

--- FreshTrack_App/app/index.jsx
+++ FreshTrack_App/app/index.jsx
@@ -1,10 +1,12 @@
 import { StyleSheet, Text, View } from 'react-native'
+import { Link } from 'expo-router'
 import React from 'react'
 
 const Home = () => {
   return (
     <View style={styles.container}>
       <Text>Landing Page</Text>
+      <Link href={"/about"}>Hey Link</Link>
     </View>
   )
 }
