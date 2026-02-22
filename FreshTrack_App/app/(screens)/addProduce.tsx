35df9dc148181a0050aadaa7e14a038edff08ca5
DuckMcQuacks <118669854+DuckMcQuacks@users.noreply.github.com>
2026-02-22 21:33:31 +0100

    Added basic frame for storing produce,

--- FreshTrack_App/app/(screens)/addProduce.tsx
+++ FreshTrack_App/app/(screens)/addProduce.tsx
@@ -1,27 +1,37 @@
 import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
-import { useSearchParams } from 'expo-router/build/hooks'
+import React, { useMemo } from 'react'
+import { useLocalSearchParams } from 'expo-router'
+import produceType from "../../assets/produceType.json"
+import { ProduceType } from '@/typeScriptComponents/ProduceType'
 
-const produceGuide = () => {
-const params = useSearchParams();
-  const itemIdStr = params.get("itemId"); // string | null
-  const itemId = itemIdStr ? Number(itemIdStr) : null;
+const ProduceGuide = () => {
 
-  if (itemId === null) {
+  const { itemId } = useLocalSearchParams<{ itemId?: string }>()
+
+  const produce = useMemo(() => {
+    if (!itemId) return null
+
+    const id = Number(itemId)
+
+    return produceType.produce.find(p => p.id === id) ?? null
+  }, [itemId])
+
+  if (!produce) {
     return (
       <View style={{ padding: 16 }}>
-        <Text>Error: No item ID provided</Text>
+        <Text>Produce not found</Text>
       </View>
-    );
+    )
   }
 
   return (
     <View style={{ padding: 16 }}>
-      <Text>Editing produce with ID: {itemId}</Text>
+      <Text>{produce.name}</Text>
+      <Text>{}</Text>
     </View>
-  );
+  )
 }
 
-export default produceGuide
+export default ProduceGuide
 
 const styles = StyleSheet.create({})
---
+++ FreshTrack_App/typeScriptComponents/StoredProduce.ts
@@ -0,0 +1,9 @@
+export interface StoredProduce {
+  id: number;
+  produceTypeId: number;
+  Year: number;
+  Month: number;
+  Day: number;
+  Hour: number;
+  Minute: number;
+}
